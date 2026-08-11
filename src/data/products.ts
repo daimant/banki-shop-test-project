01af92d77435cef84c8d48eb30ac36fdb084ef9e
Sergey Pomortsev <zaooooza@gmail.com>
2026-08-11 11:20:44 +0500

    added extra two pic in every product

--- src/data/products.ts
+++ src/data/products.ts
@@ -1,9 +1,20 @@
 import { Product } from '@/types';
 
-import product1Img from '@/assets/product-1.png';
-import product2Img from '@/assets/product-2.png';
-import product3Img from '@/assets/product-3.png';
-import product4Img from '@/assets/product-4.png';
+import product1_1 from '@/assets/product-1.jpg';
+import product1_2 from '@/assets/product-1-2.jpg';
+import product1_3 from '@/assets/product-1-3.jpg';
+
+import product2_1 from '@/assets/product-2.jpg';
+import product2_2 from '@/assets/product-2-2.jpg';
+import product2_3 from '@/assets/product-2-3.jpg';
+
+import product3_1 from '@/assets/product-3.png';
+import product3_2 from '@/assets/product-3-2.png';
+import product3_3 from '@/assets/product-3-3.png';
+
+import product4_1 from '@/assets/product-4.jpg';
+import product4_2 from '@/assets/product-4-2.png';
+import product4_3 from '@/assets/product-4-3.png';
 
 export const products: Product[] = [
   {
@@ -13,7 +24,7 @@ export const products: Product[] = [
     oldPrice: 2000000,
     description:
       '«Рождение Венеры» — картина итальянского художника Сандро Боттичелли, написанная около 1485 года. На полотне изображена богиня Венера, появляющаяся из морской пены. Картина хранится в галерее Уффици во Флоренции.',
-    images: [product1Img, product1Img, product1Img],
+    images: [product1_1, product1_2, product1_3],
     soldOut: false,
   },
   {
@@ -23,7 +34,7 @@ export const products: Product[] = [
     oldPrice: null,
     description:
       '«Тайная вечеря» — монументальная роспись Леонардо да Винчи, созданная в 1495–1498 годах. Изображает сцену последней трапезы Христа со своими учениками. Находится в монастыре Санта-Мария-делле-Грацие в Милане.',
-    images: [product2Img, product2Img, product2Img],
+    images: [product2_1, product2_2, product2_3],
     soldOut: false,
   },
   {
@@ -33,7 +44,7 @@ export const products: Product[] = [
     oldPrice: 6000000,
     description:
       '«Сотворение Адама» — фреска Микеланджело Буонарроти, созданная около 1511 года. Является частью росписи потолка Сикстинской капеллы в Ватикане. Изображает библейский сюжет сотворения первого человека.',
-    images: [product3Img, product3Img, product3Img],
+    images: [product3_1, product3_2, product3_3],
     soldOut: false,
   },
   {
@@ -43,7 +54,7 @@ export const products: Product[] = [
     oldPrice: null,
     description:
       '«Урок анатомии доктора Тульпа» — картина Рембрандта, написанная в 1632 году. Групповой портрет, изображающий публичное анатомическое вскрытие. Хранится в музее Маурицхёйс в Гааге.',
-    images: [product4Img, product4Img, product4Img],
+    images: [product4_1, product4_2, product4_3],
     soldOut: true,
   },
 ];
