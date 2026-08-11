af05c40c65eb3e715f5ee2c1c343186ab60daf06
Sergey Pomortsev <zaooooza@gmail.com>
2026-08-11 11:24:30 +0500

    removed vite

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,6 +1,19 @@
-/// <reference types="vite/client" />
-
 declare module '*.vue' {
   import Vue from 'vue';
   export default Vue;
+}
+
+declare module '*.png' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.jpg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.svg' {
+  const value: string;
+  export default value;
 }
